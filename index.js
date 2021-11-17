d86f6b98ddece1a1f35934e58648cea0eafa784c
AkenEagle <akeneagle@gmail.com>
2021-11-17 20:42:36 +0000

    Initial readme generator script

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hello world");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const questions = [
+  // Title
+  {
+    name: "title",
+    message: "What's the title of your project?",
+    type: "input",
+  },
+];
+
+const createTitleText = (title) => {
+  return `# ${title}`;
+};
+
+const createReadmeText = (answers) => {
+  return `${createTitleText(answers.title)}`;
+};
+
+const init = async () => {
+  // Prompt questions
+  const answers = await inquirer.prompt(questions);
+  // Generate readme text
+  const readmeText = createReadmeText(answers);
+  // Create readme
+  fs.writeFile("generated-readme.md", `${readmeText}`, (err) => {
+    if (err) throw err;
+    console.log("Your readme has been generated succesfully.");
+  });
+};
+
+init();
